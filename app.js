91a401bd09690979d96410bee740c4cc7344ef3b
deepak <maurya9@gmail.com>
2023-10-05 10:43:35 +0530

    js file update

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const User = require('./model/user')
 const express = require('express')
 const bcrypt = ('bcryptjs')
 const res = require('express/lib/response.js')
-const user = require('./model/user')
 const bcryptjs = require('bcryptjs')
+// const user = require('./model/user')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 
@@ -22,30 +22,30 @@ app.post("/register",async (req, res) =>{
         if (!(firstname && lastname && email && password)){
             res.status(400).send('All feilds are necessary')
         }
-        const existingUser = await user.findOne({email})
+        const existingUser = await User.findOne({email})
         if (existingUser){
             res.status(401).send('Duplicate email entry found')
         }
         
         const myEncPassword = await bcryptjs.hash(password, 10)
 
-        const user = await User.create({
+        const User = await User.create({
             firstname,
             lastname,
             email,
             password: myEncPassword
         })
         const token = jwt.sign(
-            {id: user._id, email},
+            {id: User._id, email},
             'shhhh',
             {
                 expiresIn: "2h"
             }
         );
-        user.token = token
-        user.password = undefined
+        User.token = token
+        User.password = undefined
         
-        res.status(201).json(user)
+        res.status(201).json(User)
         
     }
     catch(error){
@@ -53,44 +53,44 @@ app.post("/register",async (req, res) =>{
     }
 })
 
-app.post('/login', async(req, res)=> {
-    try {
-        //to get data from user
-        const {email, password} = req.body
-        // password validation
-        if(!(email && password)){
-            res.status(400).send('All data not entered')
-        }
-        // find user in DB
-        const user = await user.findOne({email})
-        // if user not here then proceed
+// app.post('/login', async(req, res)=> {
+//     try {
+//         //to get data from user
+//         const {email, password} = req.body
+//         // password validation
+//         if(!(email && password)){
+//             res.status(400).send('All data not entered')
+//         }
+//         // find user in DB
+//         const user = await user.findOne({email})
+//         // if user not here then proceed
         
-        //match the password
-        if (user && (await bcrypt.compare(password, user.password))){
-            const token = jwt.sign(
-                {id: user._id},
-                'shhhh',
-            {
-                expiresIn: "2h"
-            }
-            );
+//         //match the password
+//         if (user && (await bcrypt.compare(password, user.password))){
+//             const token = jwt.sign(
+//                 {id: user._id},
+//                 'shhhh',
+//             {
+//                 expiresIn: "2h"
+//             }
+//             );
 
-            user.token = token
-            user.password= undefined
-            // send parser in cookie
-            const options = {
-                expires: new Date(Date.now()+3*24*60*60*1000),
-                httpOnly: true                    
-            };
-            res.status(200).cookie("token", token, options).json({
-                success: true,
-                token,
-                user
-            })
-        }
-    } catch (error) {
-        console.log(error);
-    }
-})
+//             user.token = token
+//             user.password= undefined
+//             // send parser in cookie
+//             const options = {
+//                 expires: new Date(Date.now()+3*24*60*60*1000),
+//                 httpOnly: true                    
+//             };
+//             res.status(200).cookie("token", token, options).json({
+//                 success: true,
+//                 token,
+//                 user
+//             })
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
 
 module.exports = app
--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
-const {MONGODB_URL} = process.env
+// const MONGODB_URL = 'mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test';
 
 exports.connect = () => {
     // console.log(typeof MONGODB_URL);
-    mongoose.connect(MONGODB_URL, {
+    mongoose.connect("mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } )
