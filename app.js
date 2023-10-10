c0c75eef9fa26d4ac2384561922427813aac630a
deepak <maurya9@gmail.com>
2023-10-10 22:06:36 +0530

    Register successful

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 require("./database/database.js").connect()
-const User = require('./model/user')
 const express = require('express')
 const bcrypt = ('bcryptjs')
 const res = require('express/lib/response.js')
@@ -8,6 +7,7 @@ const bcryptjs = require('bcryptjs')
 // const user = require('./model/user')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const user = require('./model/user')
 
 const app = express()
 app.use(express.json())
@@ -22,28 +22,28 @@ app.post("/register",async (req, res) =>{
         if (!(firstname && lastname && email && password)){
             res.status(400).send('All feilds are necessary')
         }
-        const existingUser = await User.findOne({email})
+        const existingUser = await user.findOne({email})
         if (existingUser){
             res.status(401).send('Duplicate email entry found')
         }
         
         const myEncPassword = await bcryptjs.hash(password, 10)
 
-        const User = await User.create({
+        const User = await user.create({
             firstname,
             lastname,
             email,
             password: myEncPassword
         })
         const token = jwt.sign(
-            {id: User._id, email},
+            {id: user._id, email},
             'shhhh',
             {
                 expiresIn: "2h"
             }
         );
-        User.token = token
-        User.password = undefined
+        user.token = token
+        user.password = undefined
         
         res.status(201).json(User)
         
@@ -51,46 +51,49 @@ app.post("/register",async (req, res) =>{
     catch(error){
         console.log(error);
     }
-})
+});
 
-// app.post('/login', async(req, res)=> {
-//     try {
-//         //to get data from user
-//         const {email, password} = req.body
-//         // password validation
-//         if(!(email && password)){
-//             res.status(400).send('All data not entered')
-//         }
-//         // find user in DB
-//         const user = await user.findOne({email})
-//         // if user not here then proceed
+app.post('/login', async(req, res)=> {
+    try {
+        //to get data from user
+        const {email, password} = req.body
+        // password validation
         
-//         //match the password
-//         if (user && (await bcrypt.compare(password, user.password))){
-//             const token = jwt.sign(
-//                 {id: user._id},
-//                 'shhhh',
-//             {
-//                 expiresIn: "2h"
-//             }
-//             );
+        if(!(email && password)){
+            res.status(400).send('All data not entered')
+        }
 
-//             user.token = token
-//             user.password= undefined
-//             // send parser in cookie
-//             const options = {
-//                 expires: new Date(Date.now()+3*24*60*60*1000),
-//                 httpOnly: true                    
-//             };
-//             res.status(200).cookie("token", token, options).json({
-//                 success: true,
-//                 token,
-//                 user
-//             })
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
+        // find user in DB
+        const user = await user.findOne({email});
+        alert(user);
+        // if user not here then proceed
+        
+        //match the password
+        if (user && (await bcrypt.compare(password, user.password))){
+            const token = jwt.sign(
+                {id: user._id},
+                'shhhh',
+            {
+                expiresIn: "2h"
+            }
+            );
+
+            user.token = token
+            user.password= undefined
+            // send parser in cookie
+            const options = {
+                expires: new Date(Date.now()+3*24*60*60*1000),
+                httpOnly: true                    
+            };
+            res.status(200).cookie("token", token, options).json({
+                success: true,
+                token,
+                user
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = app
--- database/database.js
+++ database/database.js
@@ -1,19 +1,49 @@
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+// const Router = require("./routes")
+
 
 // const MONGODB_URL = 'mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test';
 
 exports.connect = () => {
+
+const app = express();
+
+app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test',
+  {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
     // console.log(typeof MONGODB_URL);
-    mongoose.connect("mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    } )
-    .then(
-        console.log(`DB connected Successfully`)
-    )
-    .catch((error) => {
-        console.log(`DB connection FAILED`);
-        console.log(error);
-        process.exit(1)
-    })
+    // mongoose.connect(
+    //     `mongodb+srv://Shivam_kushwaha:Shivam123@cluster0.rpkcw2e.mongodb.net/test?retryWrites=true&w=majority`, 
+    //     {
+    //       useNewUrlParser: true,
+    //       useFindAndModify: false,
+    //       useUnifiedTopology: true
+    //     } )
+    // .then(
+    //     console.log(`DB connected Successfully`)
+    // )
+    // .catch((error) => {
+    //     console.log(`DB connection FAILED`);
+    //     console.log(error);
+    //     process.exit(1)
+    // })
+
+
 }
+
+
